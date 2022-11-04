a19097550ac60642acd748b9b7f65bd1a0f0c2cc
Vytenis Petrauskas <vytenis.p@oxylabs.io>
2022-11-04 16:47:06 +0200

    Initial Commit

---
+++ JunctionApp/JunctionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunctionAppApp.swift
+//  JunctionApp
+//
+//  Created by Vytenis Petrauskas on 2022-11-04.
+//
+
+import SwiftUI
+
+@main
+struct JunctionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
