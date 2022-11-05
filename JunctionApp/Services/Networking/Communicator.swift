550c7d7947a1fe373a1966813269036fd37cec8b
Vytenis Petrauskas <visak.pet0@gmail.com>
2022-11-06 01:23:59 +0200

    Implement device scheduling

--- JunctionApp/Services/Networking/Communicator.swift
+++ JunctionApp/Services/Networking/Communicator.swift
@@ -22,10 +22,30 @@ class Communicator: CommunicatorProtocol {
         }
     }
 
-    func getDeviceConsumoption(id: Int, of date: Date) async -> DailyConsumptionDto? {
+    func getDeviceConsumption(id: Int, of date: Date) async -> DailyConsumptionDto? {
         await withCheckedContinuation { cont in
             let endpoint = "\(apiUrl)/stats/hourly/device/\(id)?start=\(date.formattedDate)"
             AF.request(endpoint, method: .get).responseDecodable(of: DailyConsumptionDto.self) { response in
+                cont.resume(returning: response.value)
+            }
+        }
+    }
+
+    func scheduleDevice(id: Int, time: Int, state: Bool) async -> ScheduleDto? {
+        await withCheckedContinuation { cont in
+            let endpoint = "\(apiUrl)/schedule"
+            let body: [String: Int] = [
+                "device_id": id,
+                "state": state ? 1 : 0,
+                "start": time
+            ]
+
+            AF.request(
+                endpoint,
+                method: .post,
+                parameters: body,
+                encoder: JSONParameterEncoder.default
+            ).responseDecodable(of: ScheduleDto.self) { response in
                 cont.resume(returning: response.value)
             }
         }
